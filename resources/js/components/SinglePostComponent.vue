08f3a686621c6ac77275106a5a9d085a8b78154d
VA-Freelancer <vproger@vk.com>
2022-12-25 13:50:02 +0400

    Initial commit

---
+++ resources/js/components/SinglePostComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        Single post
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "SinglePostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
