0c582148cf61a741796c9937d112ed5394e643b6
VA-Freelancer <vproger@vk.com>
2023-01-04 02:49:09 +0400

    upd: add delete

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -7,16 +7,19 @@
                     <th scope="col">Age</th>
                     <th scope="col">Job</th>
                     <th scope="col">Edit</th>
+                    <th scope="col">Show</th>
+                    <th scope="col">Delete</th>
                 </tr>
             </thead>
             <tbody>
                 <template v-for="person in people">
                     <tr>
-                        <td>{{ person.name}}</td>
+                        <td><router-link :to="{name: 'person.show', params: {id: person.id}}">{{ person.name}}</router-link></td>
                         <td>{{ person.age}}</td>
                         <td>{{ person.job}}</td>
-                        <td><router-link :to="{name: 'person.edit', params: {id: person.id}}">Edit</router-link></td>
-                        <td><router-link :to="{name: 'person.show', params: {id: person.id}}">Detail</router-link></td>
+                        <td><router-link :to="{name: 'person.edit', params: {id: person.id}}" class="btn btn-outline-success">Edit</router-link></td>
+                        <td><router-link :to="{name: 'person.show', params: {id: person.id}}" class="btn btn-outline-primary">Detail</router-link></td>
+                        <td><a @click.prevent="deletePerson(person.id)" href="#" class="btn btn-outline-danger">Delete</a></td>
                     </tr>
                 </template>
             </tbody>
@@ -42,6 +45,12 @@ export default {
 
                     this.people = res.data
                     console.log(this.people)
+                })
+        },
+        deletePerson(id){
+            axios.delete('/api/people/' + id)
+                .then(res => {
+                    this.getPeople()
                 })
         }
     }
